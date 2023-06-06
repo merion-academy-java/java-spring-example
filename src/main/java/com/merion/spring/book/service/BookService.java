e5c8e82d3bdce047fb28ce411942bfe8e9a80b0c
pocteg <pocteg@gmail.com>
2023-06-06 05:59:04 +0300

    Spring MVC. Post запросы

--- src/main/java/com/merion/spring/book/service/BookService.java
+++ src/main/java/com/merion/spring/book/service/BookService.java
@@ -21,7 +21,7 @@ public class BookService {
         for (int i = 0; i < 100; i++) {
             BookEntity book = new BookEntity();
             book.setId(i);
-            book.setTitle("Book #"+random.nextInt(100,999));
+            book.setTitle("Book #" + random.nextInt(100, 999));
             book.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim mattis ex, vel ultrices augue congue quis. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia urna id turpis tincidunt, vitae suscipit nunc tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse potenti. Suspendisse non iaculis odio, eget venenatis ipsum. Sed fermentum ultrices metus, tincidunt placerat enim tempor quis. Aliquam eu quam metus. Morbi arcu dui, dictum id felis et, maximus vestibulum enim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.");
             bookStorage.add(book);
         }
@@ -31,8 +31,17 @@ public class BookService {
         return bookStorage;
     }
 
-    public Optional<BookEntity> byId(Integer id){
+    public Optional<BookEntity> byId(Integer id) {
         return bookStorage.stream().filter((bookEntity -> bookEntity.getId().equals(id))).findFirst();
+    }
+
+    public BookEntity create(String title, String description) {
+        BookEntity book = new BookEntity();
+        book.setId(bookStorage.size());
+        book.setTitle(title);
+        book.setDescription(description);
+        bookStorage.add(book);
+        return book;
     }
 
 }
